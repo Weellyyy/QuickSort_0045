7e4b5c2bd2c883b2037251a7f764eb536f7738fa
Weellyyy <wildanprtm2304@gmail.com>
2024-03-28 17:08:52 +0700

    menambahkan step 1

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -46,5 +46,49 @@ void q_short(int low, int high)
 	if (low > high) { //step 1
 		return;
 	}
+	pivot = arr[low]; //step 2
+	i = low + 1; //step 3
+	j = high; //step 4
+
+
+
+	while (i <= j) //step 10
+	{
+		//search for an element greater than pivot
+		while ((arr[i] <= pivot) && (i <= high)) //step 7
+		{
+			i++; //step 6
+			cmp_count++;
+		}
+		cmp_count++;
+		//search for an element less than or equal to pivot
+		while ((arr[j] > pivot) && (j >= low)) //step 7
+		{
+			j--; //step 8
+			cmp_count++;
+		}
+		cmp_count++;
+		if (i < j) //step 9
+		{
+			//swap the element at index i whit the element at index j
+			swap(i, j);
+		}
+	}
+
+	if (low < j) { //step 11
+		//swap the pivot element with the elemen at index j
+		swap(low, j);
+	}
+
+	//recursive call to sort the left sub array
+	q_short(low, j - 1); //step 12
+
+	//recursive call to sort the right sub array
+
+	q_short(j + 1, high); //step 13
+
+
+}
+
 
 
